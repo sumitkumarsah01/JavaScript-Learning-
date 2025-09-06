437e70ab2ec5a0b5b8a37069142f88e221960882
Sumit Kumar Sah <sumitsah9801@gmail.com>
2025-09-06 19:29:29 +0530

    Learn Strings and string methods in Deep

---
+++ Strings/script.js
@@ -0,0 +1,53 @@
+// string literals
+let str1="sumit";
+// console.log(typeof(str1));
+
+// using constructor
+let str2=new String("sumit");
+// console.log(typeof(str2));
+
+
+
+// Searching methods
+
+let text="Javascript is powerfull";
+
+// console.log(text.includes("power"));
+
+// console.log(text.startsWith("Java"));
+
+// console.log(text.endsWith("rfull"));
+
+// console.log(text.indexOf("is"));
+
+// console.log(text.lastIndexOf("c"));
+
+
+//Extracting methods
+
+let str3="Javascript";
+
+// console.log(str3.slice(1,-7));
+
+// console.log(str3.substring(3,5));
+
+
+//manipulation method
+
+let str4="   Learn js Quickly   ";
+
+// console.log(str4.trim());
+
+// console.log(" sumit".repeat(5))
+
+
+
+// String to arrays
+
+let text2="Hello,World,Js";
+
+// console.log(text2.split(","))
+
+// console.log([...text2]);
+
+
