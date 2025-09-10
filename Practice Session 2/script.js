5b8eb32969577fe30308fbcdef933f5da8a69460
Sumit Kumar Sah <sumitsah9801@gmail.com>
2025-09-10 23:03:25 +0530

    Practice js Some important question

---
+++ Practice Session 2/script.js
@@ -0,0 +1,46 @@
+// 1. write to for loop print 10 to 1
+
+// for(let i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+// 2. Use a while loop to print multiples of 3 from 3 to 30
+
+// let start=3;
+// while(start<31){
+//     console.log(start);
+//     start+=3;
+// }
+
+// 3. Write a program to calculate the sum of numbers from 1 to 100 using a loop
+// let sum=0;
+// for(let i=1;i<=100;i++){
+//     sum=sum+i
+// }
+// console.log(sum)
+
+// 4.Use a for...of loop to iterate over the string "JavaScript"
+
+// let language="javascript";
+// for (const element of language) {
+//     console.log(element);
+// }
+
+// 5.Remove duplicate values from an array;
+
+// let arr=[2,3,4,3,4,2,5,6,7,6,5,7,5];
+
+// let newArr=[...new Set(arr)];
+// console.log(newArr);
+
+// 6.Find the second largest number in an array;
+
+// let arr=[2,3,4,3,4,2,5,6,7,6,5,7,5];
+// let newArr2=[...new Set(arr)];
+
+// let ans=newArr2.sort(function(a,b){
+//     return b-a;
+// });
+
+// console.log(ans);
+// console.log(ans[1]);
